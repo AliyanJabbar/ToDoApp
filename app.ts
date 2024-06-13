1a2db199d64539a11bc987ad2e7a3614b6d3fc1e
aliyan jabbar <jabbaraliyan805@gmail.com>
2024-06-14 00:07:33 +0500

    initial commit

--- app.ts
+++ app.ts
@@ -1,30 +1,77 @@
 #!/usr/bin/env node
 
-
+import chalk from 'chalk'
 import inquirer from "inquirer";
 
-let todos = []; //This is an empty array , here we will store our todos using .push
-let condition = true; // we give the default value as true
+let todos:string[] = ["EATING","DRINKING","SLEEPING"] //This is an empty array , here we will store our todos using .push
 
-while (condition) {
-  //here we make a loop , loop is something that repeat it self until we stop it.means if the condition is true , it will repeat but if the condition is false it will come out
+let question = chalk.bgBlueBright //storing chalks for our saholiat
+let Choices = chalk.greenBright //storing chalks for our saholiat
 
-  let todoQuestions = await inquirer.prompt([
+ let func = async (todos:string[])=>{
+
+  let condition = true 
+while(condition){
+  let userInput = await inquirer.prompt(
+    {
+      name: "select",
+      type: "list",
+      message:question("WHAT DO YOU WANT TO DO?"),
+      choices :[Choices("VIEW YOUR TODO LIST"),Choices("ADD IN YOUR TODO LIST"),Choices("UPDATE YOUR TODO LIST"),Choices("DELETE FROM YOUR TODO LIST"),Choices("EXIT")]
+    }
+  )
+  if(userInput.select == Choices("VIEW YOUR TODO LIST")){
+    console.log(todos)
+  }if(userInput.select == Choices("ADD IN YOUR TODO LIST")){
+
+    let adding = await inquirer.prompt (
       {
-          name: "firstQuestion",
-          message: "What would you like to add in your todos?",
-          type: "input"
-        },
-        {
-            name: "secondQuestion",
-            message: "Would you like to add more in your todos?",
-            type: "confirm", //confirm means answer in yes or no
-            default: "true" //true means if we not answer , it will concider as yes.
-        }
-    ]);
-   
+        name: "addInTodo",
+        type: "input",
+        message: question("WHAT WOULD YOU LIKE TO ADD IN YOUR TODO LIST?")
+      }
+    )
+    todos.push(adding.addInTodo)
+    console.log(todos)
+  
+ } 
+ if(userInput.select == Choices("UPDATE YOUR TODO LIST")){
+ let updating = await inquirer.prompt(
+  {
+    name: "update",
+    type : "list",
+    message: question ("WHICH TODO, YOU WANT TO UPDATE?"),
+    choices: todos.map(val=>val) //using map method to show the elements or values stored in our todos array
+  }
+ )
+ let adding = await inquirer.prompt (
+  {
+    name: "addInTodo",
+    type: "input",
+    message: question(`WHAT DO YOU WANT TO UPDATE IN ${updating.update}`)
+  }
+)
+let newTodo = todos.filter(val=>val !== updating.update)
+todos = [...newTodo,adding.addInTodo]
+console.log(todos)
 
-  todos.push(todoQuestions.firstQuestion); //push is to add something in our array
-  console.log(todos);
-  condition = todoQuestions.secondQuestion;
+ } if (userInput.select == Choices("DELETE FROM YOUR TODO LIST")){
+  let deleting = await inquirer.prompt(
+    {
+      name: "delete",
+      type : "list",
+      message: question ("WHICH TODO, YOU WANT TO DELETE?"),
+      choices: todos.map(val=>val) //using map method to show the elements or values stored in our todos array
+    }
+   )
+   let newTodo = todos.filter(val=>val !== deleting.delete)
+todos = newTodo
+console.log(todos)
+
+ } if (userInput.select == Choices("EXIT")){
+ condition = false 
+ }
 }
+ }
+
+func(todos)
